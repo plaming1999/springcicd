c525ba389d233dad8de968da15b1b54c78a7c65d
plaming1999 <131333488+plaming1999@users.noreply.github.com>
2024-02-15 19:04:16 +0530

    Update ApiController.java
    test

--- src/main/java/com/boot/controller/ApiController.java
+++ src/main/java/com/boot/controller/ApiController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 	@GetMapping("/get")
 	public String demo() throws InterruptedException {
-		Thread.sleep(3000);
+		 Thread.sleep(3000);
 		return "result from get api";
 	}
 	@PostMapping("/post")
